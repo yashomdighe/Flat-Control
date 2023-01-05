a6a5bc4b3ff52548e15abc8704faa2b6e24307e7
Yashom Dighe <yashom7@gmail.com>
2023-01-04 21:30:39 -0500

    Generate the control profile

--- src/Flatbase_feedback_control.py
+++ src/Flatbase_feedback_control.py
@@ -2,15 +2,10 @@ import numpy as np
 import matplotlib as mpl 
 from matplotlib import pyplot as plt
 from math import atan2, cos, sin, tan, atan
-from scipy.integrate import solve_ivp, odeint
+from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 
-control1_vec = []
-control2_vec = []
-
 def model_ode(t, m0, ref, gain, tmax):
-
-    global control1_vec, control2_vec
 
     x = m0[0]
     y = m0[1]
@@ -53,9 +48,6 @@ def model_ode(t, m0, ref, gain, tmax):
     control1 = z1*cos(th) + z2*sin(th)
     control2 = atan((L/v**2)*(z2*cos(th)-z1*sin(th)))
 
-    control1_vec.append(control1)
-    control2_vec.append(control2)
-
     xdot = v*cos(th)/tmax
     ydot = v*sin(th)/tmax
     thdot = (v/L)*tan(control2)/tmax
@@ -65,6 +57,68 @@ def model_ode(t, m0, ref, gain, tmax):
 
     return mdot
 
+def get_Control(tvec, sol, ref, gain, tmax):
+    
+    control1_vec = []
+    control2_vec = []
+    xsol = sol.y[0]
+    ysol = sol.y[1]
+    thsol = sol.y[2]
+    vsol = sol.y[3] 
+    L = 1
+
+    for i in range(len(tvec)):
+        x = xsol[i]
+        y = ysol[i]
+        th = thsol[i]
+        v = vsol[i]
+        t = tvec[i]
+        if v == 0:
+            v = 0.0001
+    
+        f1 = interp1d(ref[0,:], ref[1,:])
+        f2 = interp1d(ref[0,:], ref[2,:])
+        f3 = interp1d(ref[0,:], ref[3,:])
+
+        f4 = interp1d(ref[0,:], ref[4,:])
+        f5 = interp1d(ref[0,:], ref[5,:])
+        f6= interp1d(ref[0,:], ref[6,:])
+
+        x_ref = f1(t/tmax)
+        x_refd = f2(t/tmax)
+        x_refdd = f3(t/tmax)
+
+        y_ref = f4(t/tmax)
+        y_refd = f5(t/tmax)
+        y_refdd = f6(t/tmax)
+
+        e1 = x_ref-x
+        e2 = y_ref-y
+        e1_dot = x_refd-v*cos(th)
+        e2_dot = y_refd-v*sin(th)
+
+        k1 = gain[1]
+        k2 = gain[0]
+
+        k3 = gain[1] # kd 
+        k4 = gain[0] # kp
+
+        z1 = x_refdd+ k1*e1_dot+k2*e1
+        z2 = y_refdd+ k3*e2_dot+k4*e2
+
+        control1 = z1*cos(th) + z2*sin(th)
+        control2 = atan((L/v**2)*(z2*cos(th)-z1*sin(th)))
+
+        control1_vec.append(control1)
+        control2_vec.append(control2)
+
+        xdot = v*cos(th)/tmax
+        ydot = v*sin(th)/tmax
+        thdot = (v/L)*tan(control2)/tmax
+        vdot = control1/tmax
+    
+    return np.array([control1_vec, control2_vec])
+
 
 def main():
     # 5th order polynomial
@@ -73,7 +127,7 @@ def main():
     xf = np.array([1.5,0.5,0])
     L = 1
 
-    tmax = 1
+    tmax = 20
     tvec = np.linspace(0, tmax, tmax*1001)
     tau_vec = tvec/tmax
 
@@ -99,8 +153,7 @@ def main():
     
     xddTraj = np.polyval(np.polyder(xPar,2),tau_vec)
     yddTraj = np.polyval(np.polyder(yPar,2),tau_vec)
-    a_ref = np.sqrt(np.power(xddTraj,2) + np.power(yddTraj,2))
-
+    
     thTraj = np.arctan2(ydTraj,xdTraj)
     thdTraj = np.divide(np.multiply(yddTraj, xdTraj)-np.multiply(ydTraj, xddTraj), (np.power(xdTraj,2)+np.power(ydTraj,2)))
 
@@ -109,8 +162,6 @@ def main():
 
     ref = np.array([tau_vec, xTraj, xdTraj, xddTraj, yTraj, ydTraj, yddTraj])
     gain = [1,1]
-    # control_vec = np.array([control1_vec],[control2_vec])
-
 
     m0 = [xTraj[0], yTraj[0], thTraj[0], vTraj[0]]
     kp =1
@@ -121,27 +172,37 @@ def main():
     xsol = sol.y[0]
     ysol = sol.y[1]
     thsol = sol.y[2]
-    vsol = sol.y[3]
+    vsol = sol.y[3]      
 
-    print((sol.t_events))
-
+    # Post process the solution to get the control signals
+    control = get_Control(tvec=tvec, sol= sol, ref=ref, gain=gain, tmax=tmax)  
 
     #  Plot the trajectory
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.plot(xTraj, yTraj, label = 'reference')
-    ax1.plot(sol.y[0], sol.y[1], label = 'controlled output', linewidth = 1.3 ,linestyle = 'dashed')
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    ax1.plot(xTraj, yTraj, label = 'reference trajectory')
+    ax1.plot(xsol, ysol, label = 'simulated trajectory', linewidth = 1.3 ,linestyle = 'dashed')
+    ax1.set_title("Trajectories")
     ax1.legend()
     ax1.grid()
-    ax2.plot(sol.t, vTraj, label = 'velocity reference')
-    ax2.plot(sol.t, sol.y[3], label = 'controller output (v)', linewidth = 1.3 ,linestyle = 'dashed')
-    ax2.plot(sol.t, thTraj, label = 'theta reference')
-    ax2.plot(sol.t, sol.y[2], label = 'controller output (th)', linewidth = 1.3 ,linestyle = 'dashed')
-    # ax2.plot(sol.t, thdTraj, label = 'theta_dot reference')
-    # ax2.plot(sol.t, control2_vec, label = 'controller output (thdot)', linewidth = 1.3 ,linestyle = 'dashed')
-    # ax2.plot(sol.t, vTraj, label = 'velocity reference')
-    # ax2.plot(sol.t, sol.y[3], label = 'controller output (vdot)', linewidth = 1.3 ,linestyle = 'dashed')
+    
+    # Plot v and th vs time
+    ax2.plot(sol.t, vTraj, label = 'reference v')
+    ax2.plot(sol.t, vsol, label = 'simulated v', linewidth = 1.3 ,linestyle = 'dashed')
+    ax2.plot(sol.t, thTraj, label = 'reference th')
+    ax2.plot(sol.t, thsol, label = 'simulated th', linewidth = 1.3 ,linestyle = 'dashed')
+    ax2.set_title("Velocity and Theta")
     ax2.legend()
     ax2.grid()
+
+    # Plot a and phi vs time
+    a_ref = np.divide(2*np.multiply(xdTraj,xddTraj)+2*np.multiply(ydTraj, yddTraj), 2*np.sqrt(np.power(xdTraj,2) + np.power(ydTraj,2)))
+    ax3.plot(sol.t, phiTraj, label = 'phi reference')
+    ax3.plot(sol.t, control[1], label = 'controller output (phi)', linewidth = 1.3 ,linestyle = 'dashed')
+    ax3.plot(sol.t, a_ref, label = 'vdot reference')
+    ax3.plot(sol.t, control[0], label = 'controller output (vdot)', linewidth = 1.3 ,linestyle = 'dashed')
+    ax3.set_title("Control Signals")
+    ax3.legend()
+    ax3.grid()
     plt.show()
 
 if __name__ == "__main__":
