1. Use real time trajectory 2. Fix the speed calculation 3. Add more plots

src/flat_feedback_controller.py
@@ -6,7 +6,7 @@ import tf_conversions
 
 import numpy as np
 from math import atan2, cos, sin, tan, atan, sqrt
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, CubicSpline
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
@@ -18,10 +18,14 @@ class Flat_Controller:
         self.t0 = rospy.get_time()
         self.vel_prev = 0 # 0 initial velocity
         self.t_prev = 0 # initial time
+        
         self.L = 0.324
         self.tvec = []
         self.x_pose = []
         self.y_pose = []
+        self.control1_vec = []
+        self.control2_vec = []
+        self.control3_vec = []
         #  Make a copy of the reference trajectory and construct interpolation functions
         #  to get the value of the function at any time instance t
         self.ref = ref_traj
@@ -40,26 +44,37 @@ class Flat_Controller:
 
     def traj_track(self, odom):
         self.ti = rospy.get_time() - self.t0
-        rospy.loginfo(self.t0)
+        # rospy.loginfo(self.t0)
         rospy.loginfo(self.ti)
-        rospy.loginfo(self.tmax - self.ti)
-        if self.tmax - self.ti <= 0.01:
+        # rospy.loginfo(self.tmax - self.ti)
+        if self.ti > self.tmax:
             self.drive_msg.speed = 0
             self.drive_msg.acceleration = 0
             self.drive_msg.steering_angle = 0
 
             self.driver.publish(self.drive_msg)
+            x_ref = self.ref[1,:]
+            y_ref = self.ref[4,:]
 
-            fig, ax = plt.subplots()
-            ax.plot(self.x_pose, self.y_pose, color=u'#ff7f0e')
-            ax.set_title("executed trajectory")
-            ax.grid()
+            fig, (ax1, ax2) = plt.subplots(1,2)
+            ax1.plot(x_ref, y_ref, label = "reference")
+            ax1.plot(self.x_pose, self.y_pose, label = "executed")
+            ax1.set_title("executed trajectory")
+            ax1.legend()
+            ax1.grid()
+
+            ax2.plot(self.tvec, self.control1_vec, label= "acceleration")
+            ax2.plot(self.tvec, self.control2_vec, label= "phi")
+            ax2.plot(self.tvec, self.control3_vec, label= "speed")
+            ax2.set_title("control signals")
+            ax2.legend()
+            ax2.grid()
 
             plt.show()
 
             rospy.signal_shutdown("Reached Traj end time")
         
-        self.tvec.append(self.ti)
+       
         v = odom.twist.twist.linear
         ang_vel = odom.twist.twist.angular
         pose = odom.pose.pose.position
@@ -69,13 +84,13 @@ class Flat_Controller:
         orientation_quat = odom.pose.pose.orientation
         (roll, pitch, yaw) = tf_conversions.transformations.euler_from_quaternion([orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w])
         
-        x_ref = self.f1(self.ti/self.tmax)
-        x_refd = self.f2(self.ti/self.tmax)
-        x_refdd = self.f3(self.ti/self.tmax)
+        x_ref = self.f1(self.ti)
+        x_refd = self.f2(self.ti)
+        x_refdd = self.f3(self.ti)
 
-        y_ref = self.f4(self.ti/self.tmax)
-        y_refd = self.f5(self.ti/self.tmax)
-        y_refdd = self.f6(self.ti/self.tmax)
+        y_ref = self.f4(self.ti)
+        y_refd = self.f5(self.ti)
+        y_refdd = self.f6(self.ti)
 
         e1 = x_ref-pose.x
         e2 = y_ref-pose.y
@@ -91,26 +106,24 @@ class Flat_Controller:
         z1 = x_refdd+ k1*e1_dot+k2*e1
         z2 = y_refdd+ k3*e2_dot+k4*e2
 
-        # z1 = z1/self.tmax        
-        # z2 = z2/self.tmax
-
         vel_fwd = sqrt(v.x**2 + v.y**2)
         dt = self.ti - self.t_prev
-
+        # rospy.loginfo(dt)
         control1 = (z1*cos(yaw) + z2*sin(yaw)) # Acceleration
-        control2 = atan((self.L/vel_fwd**2)*(z2*cos(yaw)-z1*sin(yaw))) # Phi
-        # control1 = control1/self.tmax
-        # control2 = control2/self.tmax
+        control2 = atan((self.L/vel_fwd**2)*(z2*cos(yaw)-z1*sin(yaw))) # Ph
+        control3 = self.vel_prev + control1*dt # numerical integration to calculate velocity
 
-        control3 = self.vel_prev + control2*dt # numerical integration to calculate velocity
-
-
-        self.drive_msg.speed = control3
         self.drive_msg.acceleration = control1
         self.drive_msg.steering_angle = control2
+        self.drive_msg.speed = control3
+
+        self.control1_vec.append(control1)
+        self.control2_vec.append(control2)
+        self.control3_vec.append(control3)
 
         self.vel_prev = control3
         self.t_prev = self.ti
+        self.tvec.append(self.ti)
 
         self.driver.publish(self.drive_msg)
 
@@ -120,15 +133,13 @@ class Flat_Controller:
 if __name__ == "__main__":
     rospy.init_node("flat_controller")
 
-    #  Create a 5th order trajectory
+    # 5th order polynomial
 
     x0 = np.array([0,0,0])
     xf = np.array([1.5,0.5,0])
     L = 0.324
 
-    tmax = 10
-    tvec = np.linspace(0, tmax, tmax*1001)
-    tau_vec = tvec/tmax
+    tau_vec = np.linspace(0, 1,1001)
 
     AmatY = np.array([[tau_vec[0]**5, tau_vec[0]**4, tau_vec[0]**3, tau_vec[0]**2, tau_vec[0]**1, tau_vec[0]**0],
                     [tau_vec[-1]**5, tau_vec[-1]**4, tau_vec[-1]**3, tau_vec[-1]**2, tau_vec[-1]**1, tau_vec[-1]**0],
@@ -144,14 +155,26 @@ if __name__ == "__main__":
     xPar = np.poly1d(np.squeeze(xPar))
     yPar = np.poly1d(np.squeeze(yPar))
 
-    xTraj = np.polyval(xPar, tau_vec)
-    yTraj = np.polyval(yPar, tau_vec)
+    xTraj_normalized = np.polyval(xPar, tau_vec)
+    yTraj_normalized = np.polyval(yPar, tau_vec)
 
-    xdTraj = np.polyval(np.polyder(xPar,1),tau_vec)
-    ydTraj = np.polyval(np.polyder(yPar,1),tau_vec)
+    tmax = 10
+    tvec = np.linspace(0, tmax, 1001)
+
+    xTrajCS = CubicSpline(tvec, xTraj_normalized)
+    yTrajCS = CubicSpline(tvec, yTraj_normalized)
+
+    xTraj = xTrajCS(tvec)
+    yTraj = yTrajCS(tvec)
+
+    # xdTraj = np.polyval(np.polyder(xPar,1),tau_vec)
+    # ydTraj = np.polyval(np.polyder(yPar,1),tau_vec)
+
+    xdTraj = xTrajCS(tvec,1)
+    ydTraj = yTrajCS(tvec, 1)
     
-    xddTraj = np.polyval(np.polyder(xPar,2),tau_vec)
-    yddTraj = np.polyval(np.polyder(yPar,2),tau_vec)
+    xddTraj = xTrajCS(tvec,2)
+    yddTraj = yTrajCS(tvec, 2)
     
     thTraj = np.arctan2(ydTraj,xdTraj)
     thdTraj = np.divide(np.multiply(yddTraj, xdTraj)-np.multiply(ydTraj, xddTraj), (np.power(xdTraj,2)+np.power(ydTraj,2)))
@@ -159,7 +182,7 @@ if __name__ == "__main__":
     vTraj = np.sqrt(np.power(xdTraj,2)+ np.power(ydTraj,2))
     phiTraj = np.arctan2(L*thdTraj, vTraj)
 
-    ref = np.array([tau_vec, xTraj, xdTraj, xddTraj, yTraj, ydTraj, yddTraj])
+    ref = np.array([tvec, xTraj, xdTraj, xddTraj, yTraj, ydTraj, yddTraj])
     gain = [1,1]
 
     controller = Flat_Controller(ref, gain, tmax)