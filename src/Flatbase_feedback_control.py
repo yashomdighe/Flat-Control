df6748f4ad6580f58b2561a832fa6b382628c2b2
Yashom Dighe <yashom7@gmail.com>
2023-01-04 20:51:31 -0500

    Unnormalise normalised time

--- src/Flatbase_feedback_control.py
+++ src/Flatbase_feedback_control.py
@@ -5,7 +5,12 @@ from math import atan2, cos, sin, tan, atan
 from scipy.integrate import solve_ivp, odeint
 from scipy.interpolate import interp1d
 
-def model_ode(t, m0, ref, gain):
+control1_vec = []
+control2_vec = []
+
+def model_ode(t, m0, ref, gain, tmax):
+
+    global control1_vec, control2_vec
 
     x = m0[0]
     y = m0[1]
@@ -23,13 +28,13 @@ def model_ode(t, m0, ref, gain):
     f5 = interp1d(ref[0,:], ref[5,:])
     f6= interp1d(ref[0,:], ref[6,:])
 
-    x_ref = f1(t)
-    x_refd = f2(t)
-    x_refdd = f3(t)
+    x_ref = f1(t/tmax)
+    x_refd = f2(t/tmax)
+    x_refdd = f3(t/tmax)
 
-    y_ref = f4(t)
-    y_refd = f5(t)
-    y_refdd = f6(t)
+    y_ref = f4(t/tmax)
+    y_refd = f5(t/tmax)
+    y_refdd = f6(t/tmax)
 
     e1 = x_ref-x
     e2 = y_ref-y
@@ -48,10 +53,13 @@ def model_ode(t, m0, ref, gain):
     control1 = z1*cos(th) + z2*sin(th)
     control2 = atan((L/v**2)*(z2*cos(th)-z1*sin(th)))
 
-    xdot = v*cos(th)
-    ydot = v*sin(th)
-    thdot = (v/L)*tan(control2)
-    vdot = control1
+    control1_vec.append(control1)
+    control2_vec.append(control2)
+
+    xdot = v*cos(th)/tmax
+    ydot = v*sin(th)/tmax
+    thdot = (v/L)*tan(control2)/tmax
+    vdot = control1/tmax
 
     mdot = [xdot, ydot, thdot, vdot]
 
@@ -67,13 +75,15 @@ def main():
 
     tmax = 1
     tvec = np.linspace(0, tmax, tmax*1001)
-    AmatY = np.array([[tvec[0]**5, tvec[0]**4, tvec[0]**3, tvec[0]**2, tvec[0]**1, tvec[0]**0],
-                    [tvec[-1]**5, tvec[-1]**4, tvec[-1]**3, tvec[-1]**2, tvec[-1]**1, tvec[-1]**0],
-                    [5*tvec[0]**4, 4*tvec[0]**3, 3*tvec[0]**2, 2*tvec[0]**1, tvec[0]**0, 0],
-                    [5*tvec[-1]**4, 4*tvec[-1]**3, 3*tvec[-1]**2, 2*tvec[-1]**1, tvec[-1]**0, 0]])
+    tau_vec = tvec/tmax
 
-    AmatX = np.array([[tvec[0]**5, tvec[0]**4, tvec[0]**3, tvec[0]**2, tvec[0]**1, tvec[0]**0],
-                    [tvec[-1]**5, tvec[-1]**4, tvec[-1]**3, tvec[-1]**2, tvec[-1]**1, tvec[-1]**0]])
+    AmatY = np.array([[tau_vec[0]**5, tau_vec[0]**4, tau_vec[0]**3, tau_vec[0]**2, tau_vec[0]**1, tau_vec[0]**0],
+                    [tau_vec[-1]**5, tau_vec[-1]**4, tau_vec[-1]**3, tau_vec[-1]**2, tau_vec[-1]**1, tau_vec[-1]**0],
+                    [5*tau_vec[0]**4, 4*tau_vec[0]**3, 3*tau_vec[0]**2, 2*tau_vec[0]**1, tau_vec[0]**0, 0],
+                    [5*tau_vec[-1]**4, 4*tau_vec[-1]**3, 3*tau_vec[-1]**2, 2*tau_vec[-1]**1, tau_vec[-1]**0, 0]])
+
+    AmatX = np.array([[tau_vec[0]**5, tau_vec[0]**4, tau_vec[0]**3, tau_vec[0]**2, tau_vec[0]**1, tau_vec[0]**0],
+                    [tau_vec[-1]**5, tau_vec[-1]**4, tau_vec[-1]**3, tau_vec[-1]**2, tau_vec[-1]**1, tau_vec[-1]**0]])
 
     xPar = np.matmul(np.linalg.pinv(AmatX),np.array([[x0[0]],[xf[0]]]))
     yPar = np.matmul(np.linalg.pinv(AmatY),np.array([[x0[1]],[xf[1]],[x0[2]],[xf[2]]]))
@@ -81,14 +91,14 @@ def main():
     xPar = np.poly1d(np.squeeze(xPar))
     yPar = np.poly1d(np.squeeze(yPar))
 
-    xTraj = np.polyval(xPar, tvec)
-    yTraj = np.polyval(yPar, tvec)
+    xTraj = np.polyval(xPar, tau_vec)
+    yTraj = np.polyval(yPar, tau_vec)
 
-    xdTraj = np.polyval(np.polyder(xPar,1),tvec)
-    ydTraj = np.polyval(np.polyder(yPar,1),tvec)
+    xdTraj = np.polyval(np.polyder(xPar,1),tau_vec)
+    ydTraj = np.polyval(np.polyder(yPar,1),tau_vec)
     
-    xddTraj = np.polyval(np.polyder(xPar,2),tvec)
-    yddTraj = np.polyval(np.polyder(yPar,2),tvec)
+    xddTraj = np.polyval(np.polyder(xPar,2),tau_vec)
+    yddTraj = np.polyval(np.polyder(yPar,2),tau_vec)
     a_ref = np.sqrt(np.power(xddTraj,2) + np.power(yddTraj,2))
 
     thTraj = np.arctan2(ydTraj,xdTraj)
@@ -97,15 +107,23 @@ def main():
     vTraj = np.sqrt(np.power(xdTraj,2)+ np.power(ydTraj,2))
     phiTraj = np.arctan2(L*thdTraj, vTraj)
 
-    ref = np.array([tvec, xTraj, xdTraj, xddTraj, yTraj, ydTraj, yddTraj])
+    ref = np.array([tau_vec, xTraj, xdTraj, xddTraj, yTraj, ydTraj, yddTraj])
     gain = [1,1]
-    
+    # control_vec = np.array([control1_vec],[control2_vec])
+
+
     m0 = [xTraj[0], yTraj[0], thTraj[0], vTraj[0]]
     kp =1
     kd = 1
     gain = [kp, kd]
 
-    sol = solve_ivp(model_ode, [0,tmax], m0, t_eval=tvec ,args=(ref, gain))
+    sol = solve_ivp(model_ode, [0, tmax], m0, t_eval=tau_vec*tmax ,args=(ref, gain, tmax))
+    xsol = sol.y[0]
+    ysol = sol.y[1]
+    thsol = sol.y[2]
+    vsol = sol.y[3]
+
+    print((sol.t_events))
 
 
     #  Plot the trajectory
@@ -114,10 +132,14 @@ def main():
     ax1.plot(sol.y[0], sol.y[1], label = 'controlled output', linewidth = 1.3 ,linestyle = 'dashed')
     ax1.legend()
     ax1.grid()
-    ax2.plot(sol.t, a_ref, label = 'acceleration reference')
-    ax2.plot(sol.t, sol.y[3], label = 'controller output (vdot)', linewidth = 1.3 ,linestyle = 'dashed')
-    ax2.plot(sol.t, thdTraj, label = 'theta_dot reference')
-    ax2.plot(sol.t, sol.y[2], label = 'controller output (thdot)', linewidth = 1.3 ,linestyle = 'dashed')
+    ax2.plot(sol.t, vTraj, label = 'velocity reference')
+    ax2.plot(sol.t, sol.y[3], label = 'controller output (v)', linewidth = 1.3 ,linestyle = 'dashed')
+    ax2.plot(sol.t, thTraj, label = 'theta reference')
+    ax2.plot(sol.t, sol.y[2], label = 'controller output (th)', linewidth = 1.3 ,linestyle = 'dashed')
+    # ax2.plot(sol.t, thdTraj, label = 'theta_dot reference')
+    # ax2.plot(sol.t, control2_vec, label = 'controller output (thdot)', linewidth = 1.3 ,linestyle = 'dashed')
+    # ax2.plot(sol.t, vTraj, label = 'velocity reference')
+    # ax2.plot(sol.t, sol.y[3], label = 'controller output (vdot)', linewidth = 1.3 ,linestyle = 'dashed')
     ax2.legend()
     ax2.grid()
     plt.show()
